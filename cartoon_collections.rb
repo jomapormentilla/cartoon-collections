bbe7708707822e9424378a3cc2f74c356b4db284
jomapormentilla <jomapormentilla@gmail.com>
2020-10-21 17:16:56 -0400

    commit tree 0741ef4a1ad4f221e9807fe00febe5eb46c40e79

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves( array ) # code an argument here
   # Your code here
+  count = 1
+  array.each do |name|
+    puts "#{ count }. #{ name }\n"
+    count += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet( array ) # code an argument here
   # Your code here
+  arr = []
+  array.each do |element|
+    arr << "#{ element.capitalize }!"
+  end
+
+  arr
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls( array ) # code an argument here
   # Your code here
+  array.any?{ |call|
+    call.length > 4
+  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese( array ) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.detect{ |snack|
+    cheese_types.include?(snack)
+  }
 end
